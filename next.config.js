831f4f9e1dd8656f4ed29f553cbb9634d85dc1e5
Yusef Mosiah Nathanson <yusef@mosiah.org>
2024-03-23 18:18:19 -0400

    back to https

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
             destination:
               process.env.NODE_ENV === "development"
                 ? "http://127.0.0.1:8000/api/:path*"
-                : "http://choirchat.azurewebsites.net/api/:path*",
+                : "https://choirchat.azurewebsites.net/api/:path*",
           },
           {
             source: "/api/docs",
